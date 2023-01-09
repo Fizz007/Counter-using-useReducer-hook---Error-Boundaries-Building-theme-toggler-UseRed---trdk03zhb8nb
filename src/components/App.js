92df62e7a7fc41989dd08eb27ce066f26baff9b3
Faisal Abrar <erfaisalabrar@gmail.com>
2023-01-09 23:51:31 +0530

    updatedd2

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,55 @@
 import React, { useReducer } from 'react'
-// import { counterReducer } from '../reducers/counterReducer';
+
 import '../styles/App.css';
+
 const App = () => {
-  const initialTaskState = {
-    count: 0
-  }
-  const [taskState, dispatch] = useReducer(counterReducer, initialTaskState)
 
-  const counterReducer = (taskState, action) => {
-    switch (action.type) {
-      case 'add':
-        return { count: taskState.count + 1 }
-      case 'delete':
-        return { count: taskState.count > 0 ? taskState.count - 1 : 0 }
-  
-      case 'deleteAll':
-        return { count: 0 }
-      default:
-       throw new Error()
-    }
-  }
+const initialTaskState = { count: 0 };
 
- 
-  return (
-    <div id="main">
-      <h1>Task Counter</h1>
+const [taskState, dispatch] = useReducer(reducer, initialTaskState);
 
-      <button id='addTaskBtn' onClick={() => dispatch({ type: 'add' })}>Add a task</button>
-      <button id='delTaskBtn' onClick={() => dispatch({ type: 'delete' })}>Delete a task</button>
-      <button id='delAllTaskBtn' onClick={() => dispatch({ type: 'deleteAll' })}>Delete all tasks</button>
-  
-      <h3 className='counter'> Number of Tasks : {taskState.count}</h3>
-    </div>
-  )
+function reducer(state, action) {
+
+switch (action.type) {
+
+case 'add':
+
+return {count: state.count + 1};
+
+case 'delete':
+
+return {count: state.count > 0 ? state.count - 1 : 0};
+
+case 'deleteAll':
+
+return {count: 0};
+
+default:
+
+throw new Error();
+
 }
 
+}
+
+return (
+
+<div id="main">
+
+<h2>Task Counter</h2>
+
+<button onClick={() => dispatch({type: 'add'})} id="addTaskBtn">Add a task</button>
+
+<button onClick={() => dispatch({type: 'delete'})} id="delTaskBtn">Delete a task</button>
+
+<button onClick={() => dispatch({type: 'deleteAll'})} id="delAllTaskBtn">Delete all tasks</button>
+
+<h3 className="counter">Number of Tasks : {taskState.count}</h3>
+
+</div>
+
+)
+
+}
 
 export default App;
