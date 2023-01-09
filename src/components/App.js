9b320913d96c7be92512c40ca4f96d1318960972
Faisal Abrar <erfaisalabrar@gmail.com>
2023-01-09 23:10:03 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,34 @@ import React, { useReducer } from 'react'
 import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
 const App = () => {
-  const initialState = {
-  count:0
+  const initialTaskState = {
+    count: 0
   }
-const [state,dispatch] = useReducer(counterReducer,initialState)
+
+  // const counter = (taskState, action) => {
+  //   switch (action.type) {
+  //     case 'increment':
+  //       return { count: taskState.count + 1 }
+  //     case 'decrement':
+  //       return { count: taskState.count - 1 }
+
+  //     case 'reset':
+  //       return { count: taskState.count = 0 }
+  //     default:
+  //       return taskState;
+  //   }
+  // }
+
+  const [taskState, dispatch] = useReducer(counterReducer, initialTaskState)
   return (
     <div id="main">
-<button id='increment-btn' onClick={() => dispatch({type:'increment'})}>+</button>
-      <span id='counter'>{state.count}</span>
-      <button id='decrement-btn' onClick={() => dispatch({type:'decrement'})}>-</button>
+      <h1>Task Counter</h1>
+
+      <button id='addTaskBtn' onClick={() => dispatch({ type: 'increment' })}>Add a task</button>
+      <button id='delTaskBtn' onClick={() => dispatch({ type: 'decrement' })}>Delete a task</button>
+      <button id='delAllTaskBtn' onClick={() => dispatch({ type: 'reset' })}>Delete all tasks</button>
+      <br /><br />
+      <span id='counter' style={{ fontSize: '26px', fontWeight: 'bold' }}> Number of Tasks : {taskState.count}</span>
     </div>
   )
 }
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,12 +1,15 @@
-const counterReducer = (state,action) =>{
-    switch(action.type) {
-            case 'increment' :
-              return {count: state.count + 1}
-            case 'decrement' :
-              return {count: state.count - 1}
-            default :
-            throw new Error();
-          }
-    }
-    
-    export {counterReducer}
+const counterReducer = (taskState, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: taskState.count + 1 }
+    case 'decrement':
+      return { count: taskState.count - 1 }
+
+    case 'reset':
+      return { count: taskState.count = 0 }
+    default:
+      return taskState
+  }
+}
+
+export { counterReducer }
